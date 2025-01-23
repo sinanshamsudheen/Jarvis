ea1c36e8cdf44052e790f5d3f47cb376629c620a
sinanshamsudheen <carreersins@gmail.com>
2025-01-23 19:35:10 +0530

    completed

--- Backend/Chatbot.py
+++ Backend/Chatbot.py
@@ -1,4 +1,4 @@
-from groq import Grop
+from groq import Groq
 from json import load,dump
 import datetime
 from dotenv import dotenv_values
@@ -13,7 +13,11 @@ client=Groq(api_key=GroqAPIKey)
 
 messages=[]
 
-System=""""""
+System = f"""Hello, I am {Username}, You are a very accurate and advanced AI chatbot named {Assistantname} which also has real-time up-to-date information from the internet.
+*** Do not tell time until I ask, do not talk too much, just answer the question.***
+*** Reply in only English, even if the question is in Hindi, reply in English.***
+*** Do not provide notes in the output, just answer the question and never mention your training data. ***
+"""
 
 SystemChatBot=[
     {"role":"system","content":System}
@@ -47,3 +51,42 @@ def AnswerModifier(Answer):
     return modified_answer
 
 def ChatBot(Query):
+    try:
+        with open(r"Data\ChatLog.json","r") as f:
+            messages=load(f)
+        messages.append({"role": "user","content":f"{Query}"})
+
+        completion = client.chat.completions.create( 
+            model="llama3-70b-8192",
+            messages=SystemChatBot + [{"role":"system","content":RealtimeInformation()}] + messages,
+            max_tokens=1024,
+            temperature=0.7,
+            top_p=1,
+            stream=True,
+            stop=None
+        )
+
+        Answer=""
+
+        for chunk in completion:
+            if chunk.choices[0].delta.content:
+                Answer+=chunk.choices[0].delta.content
+        Answer=Answer.replace("</s>","")
+
+        messages.append({"role":"assistant","content":Answer})
+
+        with open(r"Data\ChatLog.json","w") as f:
+            dump(messages,f,indent=4)
+        
+        return AnswerModifier(Answer=Answer)
+    except Exception as e:
+        print(f"Error: {e}")
+        with open(r"Data\ChatLog.json","w") as f:
+            dump([],f,indent=4)
+
+if __name__ == "__main__":
+    while True:
+        user_input=input("Enter your Questions: ")
+        print(ChatBot(user_input))
+        if "bye" or "exit" in user_input:
+            break
